ed6d181957c30ed34d56afc2f173c2e686828410
Michaël Zasso <mic.besace@gmail.com>
2015-05-18 15:45:11 +0200

    fix test

--- test.js
+++ test.js
@@ -2,10 +2,11 @@ function log(m) {
     console.log(m);
 }
 
+//var SP = require('cheminfo-spectrophotometer');
 var SP = require('.');
 
 var sp = new SP('/dev/ttyACM0');
 sp._open.catch(function(e){console.log(e)});
 
-sp._send('t').then(log);
+sp.send('s').then(log);
 sp.getHelp().then(log);
